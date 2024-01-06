adf1c6174efae680ca9a480a4ed993f9fcb411aa
safderun <burakberkkeskin@gmail.com>
2024-01-06 12:39:55 +0300

    added e2e tests for provider

--- cmd/template_test.go
+++ cmd/template_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// run with --output custom-path.yaml and check if exit code is 0
 func TestHappyPathTemplateCommand(t *testing.T) {
 	command := rootCmd
 	customYamlPath := "./custom-path.yaml"
@@ -21,7 +20,6 @@ func TestHappyPathTemplateCommand(t *testing.T) {
 	os.Remove(customYamlPath)
 }
 
-// run with --output /non/existing/path and check if exit code is non zero
 func TestInvalidPathTemplateCommand(t *testing.T) {
 	command := rootCmd
 	customYamlPath := "/non/existing/path"
@@ -36,6 +34,105 @@ func TestInvalidPathTemplateCommand(t *testing.T) {
 	err := command.Execute()
 	assert.NotNil(t, err)
 	assert.Contains(t, b.String(), expected)
+	os.Remove(customYamlPath)
+}
+
+func TestDefaultTemplateProvider(t *testing.T) {
+	command := rootCmd
+	customYamlPath := "./test.yaml"
+	command.SetArgs([]string{
+		"template",
+		"--output", customYamlPath,
+	})
+	b := bytes.NewBufferString("")
+	command.SetOut(b)
+	command.SetErr(b)
+	err := command.Execute()
+	assert.Nil(t, err)
+	data, err := os.ReadFile(customYamlPath)
+	fileContent := string(data)
+	assert.Nil(t, err)
+	assert.Contains(t, fileContent, "username: ''")
+	assert.Contains(t, fileContent, "host: '127.0.0.1'")
+	assert.Contains(t, fileContent, "port: 1025")
+	assert.Contains(t, fileContent, "ssl: false")
+	assert.Contains(t, fileContent, "tls: false")
+	assert.Contains(t, fileContent, "auth: 'NO'")
+	os.Remove(customYamlPath)
+}
+
+func TestGmailTemplateProvider(t *testing.T) {
+	command := rootCmd
+	customYamlPath := "./test.yaml"
+	command.SetArgs([]string{
+		"template",
+		"--output", customYamlPath,
+		"--provider", "gmail",
+	})
+	b := bytes.NewBufferString("")
+	command.SetOut(b)
+	command.SetErr(b)
+	err := command.Execute()
+	assert.Nil(t, err)
+	data, err := os.ReadFile(customYamlPath)
+	fileContent := string(data)
+	assert.Nil(t, err)
+	assert.Contains(t, fileContent, "username: 'from@gmail.com'")
+	assert.Contains(t, fileContent, "host: 'smtp.gmail.com'")
+	assert.Contains(t, fileContent, "port: 587")
+	assert.Contains(t, fileContent, "ssl: false")
+	assert.Contains(t, fileContent, "tls: true")
+	assert.Contains(t, fileContent, "auth: 'LOGIN'")
+	os.Remove(customYamlPath)
+}
+
+func TestYandexTemplateProvider(t *testing.T) {
+	command := rootCmd
+	customYamlPath := "./test.yaml"
+	command.SetArgs([]string{
+		"template",
+		"--output", customYamlPath,
+		"--provider", "yandex",
+	})
+	b := bytes.NewBufferString("")
+	command.SetOut(b)
+	command.SetErr(b)
+	err := command.Execute()
+	assert.Nil(t, err)
+	data, err := os.ReadFile(customYamlPath)
+	fileContent := string(data)
+	assert.Nil(t, err)
+	assert.Contains(t, fileContent, "username: 'example@yandex.com'")
+	assert.Contains(t, fileContent, "host: 'smtp.yandex.com'")
+	assert.Contains(t, fileContent, "port: 465")
+	assert.Contains(t, fileContent, "ssl: true")
+	assert.Contains(t, fileContent, "tls: false")
+	assert.Contains(t, fileContent, "auth: 'LOGIN'")
+	os.Remove(customYamlPath)
+}
+
+func TestBrevoTemplateProvider(t *testing.T) {
+	command := rootCmd
+	customYamlPath := "./test.yaml"
+	command.SetArgs([]string{
+		"template",
+		"--output", customYamlPath,
+		"--provider", "brevo",
+	})
+	b := bytes.NewBufferString("")
+	command.SetOut(b)
+	command.SetErr(b)
+	err := command.Execute()
+	assert.Nil(t, err)
+	data, err := os.ReadFile(customYamlPath)
+	fileContent := string(data)
+	assert.Nil(t, err)
+	assert.Contains(t, fileContent, "username: 'user@example.com'")
+	assert.Contains(t, fileContent, "host: 'smtp-relay.brevo.com'")
+	assert.Contains(t, fileContent, "port: 587")
+	assert.Contains(t, fileContent, "ssl: false")
+	assert.Contains(t, fileContent, "tls: true")
+	assert.Contains(t, fileContent, "auth: 'LOGIN'")
 	os.Remove(customYamlPath)
 }
 
